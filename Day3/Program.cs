3b46d72118f3e83d1b0613a893d8784a1a385710
SteveeB92 <powcee@googlemail.com>
2016-12-04 09:27:31 +0000

    Day 3 Part 1, 2 and tests

---
+++ Day3/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3
+{
+    public class Program
+    {
+        private const string PATH = @"..\..\Day3.txt";
+
+        public static void Main(string[] args)
+        {
+            int amountOfPossibleTriangles = amountOfPossibleTrianglesPart1(PATH);
+            Console.Out.WriteLine($"Part 1. Possible Triangles: {amountOfPossibleTriangles}");
+
+            amountOfPossibleTriangles = amountOfPossibleTrianglesPart2(PATH);
+            Console.Out.WriteLine($"Part2. Possible Triangles: {amountOfPossibleTriangles}");
+
+            Console.ReadKey();
+        }
+
+        public static int amountOfPossibleTrianglesPart1(string path)
+        {
+            int amountOfPossibleTriangles = 0;
+            using (FileStream fileStream = File.Open(path, FileMode.Open, FileAccess.Read))
+            using (BufferedStream bufferedStream = new BufferedStream(fileStream))
+            using (StreamReader streamReader = new StreamReader(bufferedStream))
+            {
+                while (!streamReader.EndOfStream)
+                {
+                    string line = streamReader.ReadLine();
+                    line = removeExcessSpaces(line);
+                    string[] lengths = line.Split(' ');
+
+                    if (isPossibleTriangle(lengths[0], lengths[1], lengths[2]))
+                        amountOfPossibleTriangles++;
+                }
+            }
+            return amountOfPossibleTriangles;
+        }
+
+
+        public static int amountOfPossibleTrianglesPart2(string path)
+        {
+            int amountOfPossibleTriangles = 0;
+            using (FileStream fileStream = File.Open(path, FileMode.Open, FileAccess.Read))
+            using (BufferedStream bufferedStream = new BufferedStream(fileStream))
+            using (StreamReader streamReader = new StreamReader(bufferedStream))
+            {
+                while (!streamReader.EndOfStream)
+                {
+                    /*  Format:
+                     *  1  2  3
+                     *  1  2  3
+                     *  1  2  3
+                     *  4  5  6
+                     *  .  .  .
+                     */
+                    string[][] columns = new string[3][];
+                    for (int i = 0; i < 3; i++)
+                    {
+                        string line = streamReader.ReadLine();
+                        line = removeExcessSpaces(line);
+                        string[] lengths = line.Split(' ');
+                        columns[i] = lengths;
+                    }
+
+                    for (int x = 0; x < 3; x++)
+                    {
+                        amountOfPossibleTriangles += isPossibleTriangle(columns[0][x], columns[1][x], columns[2][x]) ? 1 : 0;
+                    }
+                }
+            }
+            return amountOfPossibleTriangles;
+        }
+
+        public static string removeExcessSpaces(string line)
+        {
+            return line.TrimStart().Replace("   ", " ").Replace("  ", " ");
+        }
+
+        public static bool isPossibleTriangle(string firstSideString, string secondSideString, string thirdSideString)
+        {
+            int firstSide = int.Parse(firstSideString);
+            int secondSide = int.Parse(secondSideString);
+            int thirdSide = int.Parse(thirdSideString);
+
+            //If the smaller two sides are more than the other side than this is a possible triangle
+            int smallestSidesLength = findLengthOfTwoSmallestSides(firstSide, secondSide, thirdSide);
+            int largestSideLength = findLargestSideLength(firstSide, secondSide, thirdSide);
+            return smallestSidesLength > largestSideLength;
+        }
+
+        public static int findLengthOfTwoSmallestSides(int firstSide, int secondSide, int thirdSide)
+        {
+            return Math.Min(firstSide + secondSide, Math.Min(secondSide + thirdSide, thirdSide + firstSide));
+        }
+
+        public static int findLargestSideLength(int firstSide, int secondSide, int thirdSide)
+        {
+            return Math.Max(firstSide, Math.Max(secondSide, thirdSide));
+        }
+    }
+}
---
+++ Day3Tests/ProgramTests.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Day3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+
+        private const string PATH = @"..\..\Day3.txt";
+        [TestMethod()]
+        public void amountOfPossibleTrianglesPart1Test()
+        {
+            //Just so not broken anything when refactoring
+            Assert.AreEqual(983, Program.amountOfPossibleTrianglesPart1(PATH));
+        }
+
+        [TestMethod()]
+        public void amountOfPossibleTrianglesPart2Test()
+        {
+            Assert.AreEqual(1836, Program.amountOfPossibleTrianglesPart2(PATH));
+        }
+
+        [TestMethod()]
+        public void removeExcessSpacesTest()
+        {
+            string exampleStringWithSpaces = "    4   21  894";
+            Assert.AreEqual("4 21 894", Program.removeExcessSpaces(exampleStringWithSpaces));
+        }
+
+        [TestMethod()]
+        public void isPossibleTriangleTest()
+        {
+            // Obvious fail -- 5 + 10 < 25
+            string firstSide = "5";
+            string secondSide = "10";
+            string thirdSide = "25";
+            Assert.AreEqual(false, Program.isPossibleTriangle(firstSide, secondSide, thirdSide));
+
+            // Obvious pass -- 15 + 20 > 25
+            firstSide = "15";
+            secondSide = "20";
+            thirdSide = "25";
+            Assert.AreEqual(true, Program.isPossibleTriangle(firstSide, secondSide, thirdSide));
+
+            // Different order possible triangle 
+            firstSide = "25";
+            secondSide = "15";
+            thirdSide = "20";
+            Assert.AreEqual(true, Program.isPossibleTriangle(firstSide, secondSide, thirdSide));
+        }
+
+        [TestMethod()]
+        public void findLengthOfTwoSmallestSidesTest()
+        {
+            int firstSide = 5;
+            int secondSide = 10;
+            int thirdSide = 16;
+            Assert.AreEqual(15, Program.findLengthOfTwoSmallestSides(firstSide, secondSide, thirdSide));
+
+            //Different order
+            firstSide = 16;
+            secondSide = 10;
+            thirdSide = 5;
+            Assert.AreEqual(15, Program.findLengthOfTwoSmallestSides(firstSide, secondSide, thirdSide));
+        }
+
+        [TestMethod()]
+        public void findLargestSideLengthTest()
+        {
+            int firstSide = 5;
+            int secondSide = 10;
+            int thirdSide = 16;
+            Assert.AreEqual(16, Program.findLargestSideLength(firstSide, secondSide, thirdSide));
+
+            //Different order
+            firstSide = 16;
+            secondSide = 10;
+            thirdSide = 5;
+            Assert.AreEqual(16, Program.findLargestSideLength(firstSide, secondSide, thirdSide));
+        }
+    }
+}
